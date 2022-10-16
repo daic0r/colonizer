ff2ab80018b0d57b807ecfc800d2e1fb81c5e0da
Matthias Grün <gruen.matthias@gmx.net>
2022-10-16 23:09:32 +0200

    Builds now

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <Engine.h>
 #include <System/Config.h>
 #include "Colonizer.h"
+#include <iostream>
+#include <string>
 
 int main(int argc, const char* argv[]) {
-    using namespace Ice;
+	using namespace Ice;
 
-    Config cfg{ 1980, 1020, false, 0, 0, "Data/Assets/", PathType::RELATIVE_PATH, "Data/", PathType::RELATIVE_PATH };
+    Config cfg{ 1980, 1020, false, 0, 0, "../Data/Assets/", PathType::RELATIVE_PATH, "../Data/", PathType::RELATIVE_PATH};
     cfg.setGameName("Colonizer");
     cfg.camera().m_fMaxDistance = 500.0f;
 
